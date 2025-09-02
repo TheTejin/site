acd55477c8fc4ae4fb6bcb4361256d8060c1eae5
Tejin <zadrotvbs@gmail.com>
2025-09-03 00:08:30 +0600

    add

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// Находим элементы
+
 const btn = document.getElementById("btn");
 const title = document.getElementById("title");
 
-// Делаем действие при клике
 btn.addEventListener("click", () => {
     title.textContent = "Ты гей";
     title.style.color = "red";
